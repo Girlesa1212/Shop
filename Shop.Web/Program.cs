50693617ce006abd5e94dfea1a62f1e445a6d68a
Girlesa1212 <47569670+Girlesa1212@users.noreply.github.com>
2019-03-20 17:47:03 -0500

    create SeedDB ok insert 3 products

--- Shop.Web/Program.cs
+++ Shop.Web/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace Shop.Web
 {
+
     using Data;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
@@ -9,13 +10,25 @@
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();     
-
+            var host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
         }
+
+        private static void RunSeeding(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
+        }
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-           WebHost.CreateDefaultBuilder(args)
-               .UseStartup<Startup>();
-       
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
+
 
 }
