899edeb9edfc48dee17c934e4950e9cb5600fa25
Girlesa1212 <47569670+Girlesa1212@users.noreply.github.com>
2019-03-25 13:07:00 -0500

    Add Generic Repository and countries entity

--- Shop.Web/Data/SeedDB.cs
+++ Shop.Web/Data/SeedDB.cs
@@ -5,16 +5,19 @@ namespace Shop.Web.Data
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
-    
-    
+    using Microsoft.AspNetCore.Identity;
+    using Helper;
+
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly IUserHelper userHelper;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, IUserHelper userHelper)
         {
             this.context = context;
+            this.userHelper = userHelper;
             this.random = new Random();
         }
 
@@ -22,25 +25,45 @@ namespace Shop.Web.Data
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Juan",
+                    LastName = "Zuluaga",
+                    Email = "jzuluaga55@gmail.com",
+                    UserName = "jzuluaga55@gmail.com"
+                };
+
+                var result = await this.userHelper.AddUserAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("iphone X");
-                this.AddProduct("Magic Mause");
-                this.AddProduct("iWhatch series 4");
+                this.AddProduct("iphone X", user);
+                this.AddProduct("iWatch Series 4", user);
+                this.AddProduct("Magic Mouse", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
-                Price = this.random.Next(1000),
+                Price = this.random.Next(100),
                 IsAvailabe = true,
-                Stock = this.random.Next(100)
+                Stock = this.random.Next(100),
+                User = user
             });
         }
     }
+
 
 }
